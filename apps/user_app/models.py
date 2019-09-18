746ea4dbd0fb88523eff1c205ebf64f76c278a83
Wafi AlShammari <wafi543@outlook.sa>
2019-09-18 12:23:47 +0300

    initial commit

---
+++ apps/user_app/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return f'first_name: {self.first_name}, last_name: {self.last_name}, email: {self.email}'
+
+class File(models.Model):
+    name = models.CharField(max_length=50)
+    path = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Report(models.Model):
+    file_id = models.ForeignKey(File, related_name="reports")
+    created_at = models.DateTimeField(auto_now_add=True)
+
