bd2dbc4141d34fe9ec7654b171659f380fc867a6
Abdulaziz Saud <azoz-saud@live.com>
2019-09-22 00:59:55 +0300

    Added upload file feature
    remeber before running to:
    python manage.py makemigrations
    pythone manage.py migrate

--- apps/user_app/urls.py
+++ apps/user_app/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index),
@@ -10,5 +12,10 @@ urlpatterns = [
     url(r'^profile$', views.profile),
     url(r'^update_profile$', views.update_profile),
     url(r'^add_file$', views.add_file),
+    url(r'^upload_file$', views.upload_file),
+
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
